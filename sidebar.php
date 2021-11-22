51df4451697de641aefcf8b74150350c24710ac6
nugroho62 <nugrohotriatmojo62@gmail.com>
2021-11-22 17:15:06 +0700

    tambah karyawan

--- sidebar.php
+++ sidebar.php
@@ -46,7 +46,7 @@
                              </a>
                          </li>
                          <li class="nav-item">
-                             <a href="" class="nav-link">
+                             <a href="tambah.php" class="nav-link">
                                  <i class="far fa-circle nav-icon"></i>
                                  <p>Tambah Karyawan</p>
                              </a>
